34bc8e4ad18a11267e30406e58ae6d4af71cedf1
micaporras <micalorrainedporras@iskolarngbayan.pup.edu.ph>
2021-11-13 13:31:45 +0800

    Enter 3 numbers and it will display the lowest one

---
+++ program2.py
@@ -0,0 +1,25 @@
+# Program 2: Find the lowest number
+# Create a program that ask 3 numbers. 
+# Find the lowest number using only if-else statement.
+# Display the lowest number
+
+# Function for inputing 3 numbers
+def get_numbers():
+    a = input("First Number: ")
+    b = input("Second Number: ")
+    c = input("Third Number: ")
+    return a, b, c
+
+
+# Ask for 3 number then convert and store
+a, b, c = get_numbers()
+# Test for the lowest number
+if c < a < b or c < b < a:
+    print(f"The lowest number is {c}")
+elif b < a < c or b < c < a:
+    print(f"The lowest number is {b}")
+elif a < b < c or a < c < b:
+    print(f"The lowest number is {a}")
+
+
+print("Thank you")
